4325602f883449e4961bc1ec2f0eb95471e1ccde
gurumohanreddy <gurumohan449@gmail.com>
2016-03-08 11:08:36 +0530

    mouse move implemented

--- javascript.js
+++ javascript.js
@@ -1,26 +1,59 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
-var ballSpeedX =5;
+var ballY = 50;
+var ballSpeedX =10;
+var ballSpeedY =4;
+
+var paddle1 = 250;
+const PADDLE_HEIGHT = 100;
+var paddle2 = 250;
+
+
+function calculateMousePos(evt){
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    return {
+      x:mouseX,
+      y:mouseY
+    }
+}
+
 window.onload = function(){
   canvas = document.getElementById('gamecanvas');
   canvasContext = canvas.getContext('2d');
   var framepersecond = 30;
-  setInterval(function(){
-    moveEverything();
-    drawEverything();
-  },1000/framepersecond);
+          setInterval(function(){
+            moveEverything();
+            drawEverything();
+          },1000/framepersecond);
+
+      canvas.addEventListener('mousemove',function(evt){
+              var mousePos = calculateMousePos(evt);
+              paddle1 = mousePos.y-(PADDLE_HEIGHT/2);
+      });
+
 
 }
 
 
 function moveEverything(){
   ballX = ballX +ballSpeedX;
+  ballY = ballY +ballSpeedY;
   if (ballX >canvas.width) {
     ballSpeedX = -ballSpeedX;
   }
   if (ballX <0) {
     ballSpeedX = -ballSpeedX;
+  }
+  if (ballY >canvas.height) {
+    ballSpeedY = -ballSpeedY;
+  }
+  if (ballY <0) {
+    ballSpeedY = -ballSpeedY;
   }
 }
 
@@ -29,10 +62,12 @@ function drawEverything(){
   colorRect(0,0,canvas.width,canvas.height,'black');
 
   //paddle
-  colorRect(0,100,10,100,'white');
+  colorRect(0,paddle1,10,PADDLE_HEIGHT,'white');
+  // colorRect(0,paddle2,10,100,'white');
+
 
   //ball
-  colorCircle(ballX,150,10,'white');
+  colorCircle(ballX,ballY,10,'white');
 }
 
 function colorRect(leftX,topY,width,height,drawColor){
